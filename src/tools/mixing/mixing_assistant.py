9a2b14527de3ae896eb1c4ea138da134a6ef4c34
Alen Lukic <alen@amplifydmusic.com>
2020-02-14 00:21:26 -0800

    wip

--- src/tools/mixing/mixing_assistant.py
+++ src/tools/mixing/mixing_assistant.py
@@ -104,7 +104,7 @@ class MixingAssistant:
     def rename_tracks(self):
         """ Rename tracks in tmp directories. """
         self.dm.rename_songs(join(TMP_MUSIC_DIR, 'mp3'))
-        self.dm.rename_songs(join(TMP_MUSIC_DIR, 'lossless'))
+        # self.dm.rename_songs(join(TMP_MUSIC_DIR, 'lossless'))
         print('\nSongs renamed.')
 
     def shutdown(self):
