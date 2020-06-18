511198bb28193a007906415ce0b48ca637f0c65a
Alen Lukic <alen@amplifydmusic.com>
2020-06-18 00:54:44 -0700

    wip

---
+++ src/db/utils/tag_records.py
@@ -0,0 +1,40 @@
+from src.db.entities.tag_record import *
+from src.definitions.database import TAG_COLUMNS
+from src.tools.data_management.audio_file import AudioFile
+
+
+def create_row(track):
+    audio_file = AudioFile(track.file_path)
+    row = {k: audio_file.get_tag(k) for k in TAG_COLUMNS}
+    row['track.id'] = track.id
+    return row
+
+
+def create_initial_tag_record(track, session):
+    row = create_row(track)
+    session.add(InitialTagRecord(**row))
+    session.commit()
+
+
+def create_post_mik_tag_record(track, session):
+    row = create_row(track)
+    session.add(PostMIKTagRecord(**row))
+    session.commit()
+
+
+def create_post_rekordbox_tag_record(track, session):
+    row = create_row(track)
+    session.add(PostRekordboxTagRecord(**row))
+    session.commit()
+
+
+def create_final_tag_record(track, session):
+    """
+    TODO: add consolidation logic here.
+
+    :param track:
+    :param session:
+    """
+    row = create_row(track)
+    session.add(FinalTagRecord(**row))
+    session.commit()
