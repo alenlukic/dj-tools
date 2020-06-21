edb083795c92f91bd5b3bebb78645415aaadc4b0
Alen Lukic <alen@amplifydmusic.com>
2020-06-21 01:11:39 -0700

    update camelot codes

--- src/tools/db/tag_record_pipeline.py
+++ src/tools/db/tag_record_pipeline.py
@@ -24,7 +24,7 @@ class TagRecordPipeline:
         TagRecordType.FINAL.value: 'FinalRecordFactory'
     }
 
-    def __init__(self, record_type, source_dir=PIPELINE_MUSIC_DIR, target_dir=EXPERIMENTAL_MUSIC_DIR):
+    def __init__(self, record_type, source_dir=PROCESSED_MUSIC_DIR, target_dir=EXPERIMENTAL_MUSIC_DIR):
         self.record_type = record_type
         self.session = database.create_session()
         self.source_dir = source_dir
@@ -69,22 +69,25 @@ class TagRecordPipeline:
         finally:
             self.session.close()
 
-    # def sync_final_records(self):
-    #     """ Syncs records in the final_tags table to tracks' ID3 tags and the tracks table. TODO: clean this up!"""
-    #     # final_records = {fr.title: fr for fr in self.session.query(FinalTagRecord).all()}
-    #     tracks = {t.file_path: t for t in self.session.query(Track).all()}
-    #
-    #     for track_file in self.track_files:
-    #         source_path = join(PROCESSED_MUSIC_DIR, track_file)
-    #         track = tracks[source_path]
-    #         audio_file = AudioFile(source_path)
+    def sync_final_records(self):
+        """ Syncs records in the final_tags table to tracks' ID3 tags and the tracks table. TODO: clean this up!"""
+        # final_records = {fr.title: fr for fr in self.session.query(FinalTagRecord).all()}
+        tracks = {t.file_path: t for t in self.session.query(Track).all()}
 
+        for track_file in self.track_files:
+            source_path = join(PROCESSED_MUSIC_DIR, track_file)
+            track = tracks[source_path]
+            audio_file = AudioFile(source_path)
+            #
             # title = track.title
             # record = final_records[normalize_tag_text(title)]
-
+            #
             # title_sans_md = MD_COMPOSITE_REGEX.split(title)[1].strip().split(' - ')[-1].strip()
             # bpm = float(record.bpm)
             # key = record.key
+            key = track.key
+            camelot_code = audio_file.format_camelot_code(key.lower())
+            track.camelot_code = camelot_code
             # energy = None if record.energy is None else int(record.energy)
             #
             # updated_tags = {k: v for k, v in {
@@ -93,22 +96,22 @@ class TagRecordPipeline:
             #     TrackDBCols.KEY.value: key,
             #     TrackDBCols.ENERGY.value: energy
             # }.items() if v is not None}
-
+            #
             # key = audio_file.get_tag(ID3Tag.KEY, None)
             # camelot_code = audio_file.format_camelot_code(key.lower())
             # bpm = audio_file.format_bpm()
             # title_sans_md = MD_COMPOSITE_REGEX.split(track.title)[1].strip()
             # title = ' '.join([audio_file.generate_title_prefix(camelot_code, key, bpm), title_sans_md])
-
+            #
             # updated_tags = {k: v for k, v in {
             #     TrackDBCols.TITLE.value: title,
             #     TrackDBCols.BPM.value: bpm
             # }.items() if v is not None}
             # audio_file.write_tags(updated_tags)
-
+            #
             # track.title = title
             # track.bpm = float(bpm)
-
+            #
             # comment = literal_eval(audio_file.generate_metadata()[TrackDBCols.COMMENT.value])
             # comment[TrackDBCols.TITLE.value] = title
             # comment[TrackDBCols.BPM.value] = track.bpm
@@ -116,7 +119,7 @@ class TagRecordPipeline:
             # audio_file.write_tags({TrackDBCols.COMMENT.value: track.comment})
             # track.comment = comment
 
-        # self.session.commit()
+        self.session.commit()
 
     def _load_rb_tags(self):
         rb_tag_file = CONFIG['REKORDBOX_TAG_FILE']
