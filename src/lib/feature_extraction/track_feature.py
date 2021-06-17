fv computation fix

src/lib/feature_extraction/track_feature.py
@@ -7,6 +7,7 @@ from shutil import copyfile
 from src.db.entities.feature_value import FeatureValue
 from src.definitions.feature_extraction import *
 from src.utils.feature_extraction import load_json_from_file
+from src.utils.file_operations import get_track_load_target
 
 
 class TrackFeature:
@@ -21,8 +22,9 @@ class TrackFeature:
         self.postprocessed_value = None
         self.track_features = None
 
-    def get_feature(self):
-        if self.feature_value is None:
+    def get_feature(self, compute=False):
+        if self.feature_value is None and compute:
+            print('%s missing for track %s - computing...' % (self.feature_name, self.track.title))
             self.compute()
 
         return self.feature_value
@@ -64,20 +66,32 @@ class SegmentedMeanMelSpectrogram(TrackFeature):
         self.n_mels = n_mels
 
     def preprocess(self, feature_value):
+        if feature_value is None:
+            return None
+
         if self.preprocessed_value is None:
             self.preprocessed_value = [[np.format_float_scientific(v, precision=3) for v in r] for r in feature_value]
 
         return self.preprocessed_value
 
     def postprocess(self, feature_value):
+        if feature_value is None:
+            return None
+
         if self.postprocessed_value is None:
             self.postprocessed_value = np.array([[float(v) for v in row] for row in feature_value])
 
         return self.postprocessed_value
 
+    def load(self):
+        fv = self.db_session.query(FeatureValue).filter_by(track_id=self.track.id).first()
+        if fv is not None:
+            self.feature_value = self.postprocess(fv.features.get(self.feature_name))
+
     # Compute segmented mean Mel spectrogram
     def compute(self):
-        samples, _ = librosa.load(self.track.file_path, SAMPLE_RATE)
+        target_path = get_track_load_target(self.track)
+        samples, _ = librosa.load(target_path, SAMPLE_RATE)
         n = len(samples)
 
         # Create overlapping windows
@@ -88,7 +102,7 @@ class SegmentedMeanMelSpectrogram(TrackFeature):
                 padding = end - n
                 windows.append(np.concatenate((samples[i:n], np.zeros(padding)), axis=None))
             else:
-                windows.append(samples[i:end])
+               windows.append(samples[i:end])
 
         # Calculate Mel spectrogram for each overlapping window
         window_spectrograms = [librosa.feature.melspectrogram(y=w, sr=SAMPLE_RATE, n_mels=self.n_mels) for w in windows]

src/scripts/feature_extraction/create_transition_match_rows.py
@@ -1,3 +1,4 @@
+import argparse
 from functools import lru_cache
 import numpy as np
 
@@ -7,7 +8,11 @@ from src.definitions.feature_extraction import *
 from src.lib.harmonic_mixing.transition_match_finder import TransitionMatchFinder
 from src.lib.feature_extraction.track_feature import SegmentedMeanMelSpectrogram
 from src.lib.error_management.reporting_handler import handle
+from src.utils.file_operations import stage_tracks
 from src.utils.harmonic_mixing import *
+
+
+cache_session = database.create_session()
 
 
 def get_existing_tm_id_pairs():
@@ -19,8 +24,12 @@ def generate_tm_id_pairs_to_create():
         for match in matches:
             match_track = track_file_path_map[match.metadata[TrackDBCols.FILE_PATH]]
             match_id = match_track.id
+
             if (track_id, match_id) not in existing_id_pairs:
                 pairs_to_create.add((track_id, match_id, relative_key))
+                stage_queue[match_id] = match_track
+
+    stage_queue = {}
 
     for track in tracks:
         track_id = track.id
@@ -30,46 +39,72 @@ def generate_tm_id_pairs_to_create():
         _generate_pairs(higher_key, RelativeKey.STEP_DOWN.value)
         _generate_pairs(lower_key, RelativeKey.STEP_UP.value)
 
+        stage_queue[track_id] = track
+
+    stage_tracks(stage_queue.values())
+
 
 @lru_cache(1024)
-def get_smms_value(track_id, db_session):
-    smms = SegmentedMeanMelSpectrogram(track_id_map[track_id], db_session)
+def get_smms_value(track_id):
+    smms = SegmentedMeanMelSpectrogram(track_id_map[track_id], cache_session)
     smms.load()
     return smms
 
 
-def create_transition_match_smms_rows():
-    db_session = session
-    rows_created = 0
+def create_transition_match_smms_rows(sesh, compute_missing):
+    db_session = sesh
+    num_to_create = len(pairs_to_create)
 
     try:
-        for (on_deck_id, candidate_id, relative_key) in pairs_to_create:
-            on_deck_smms = get_smms_value(on_deck_id, db_session).get_feature()
-            match_smms = get_smms_value(candidate_id, db_session).get_feature()
-            mel_score = np.linalg.norm(on_deck_smms - match_smms)
-            match_row = {
-                'on_deck_id': on_deck_id,
-                'candidate_id': candidate_id,
-                'match_factors': {Feature.SMMS.value: mel_score},
-                'relative_key': relative_key
-            }
+        for i, (on_deck_id, candidate_id, relative_key) in enumerate(pairs_to_create):
+            try:
+                on_deck_smms = get_smms_value(on_deck_id).get_feature(compute_missing)
+                if on_deck_smms is None:
+                    continue
 
-            if rows_created % 100 == 0:
-                print('%d rows created' % rows_created)
-                print('Cache info: %s\n' % str(get_smms_value.cache_info()))
+                match_smms = get_smms_value(candidate_id).get_feature(compute_missing)
+                if match_smms is None:
+                    continue
 
-            db_session = database.recreate_session_contingent(db_session)
-            if db_session.guarded_add(TransitionMatchRow(**match_row)):
-                rows_created += 1
+                mel_score = np.linalg.norm(on_deck_smms - match_smms)
+                match_row = {
+                    'on_deck_id': on_deck_id,
+                    'candidate_id': candidate_id,
+                    'match_factors': {Feature.SMMS.value: mel_score},
+                    'relative_key': relative_key
+                }
+
+                if i % 100 == 0:
+                    print('%d of %d pairs processed' % (i, num_to_create))
+                    print('Cache info: %s\n' % str(get_smms_value.cache_info()))
+
+                # noinspection PyShadowingNames, PyUnboundLocalVariable
+                db_session = database.recreate_session_contingent(db_session)
+                db_session.guarded_add(TransitionMatchRow(**match_row))
+
+            except Exception as e:
+                handle(e)
+                continue
 
     except Exception as e:
         handle(e)
 
     finally:
         db_session.close()
+        cache_session.close()
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Compute mean Mel spectrogram features.')
+    parser.add_argument('--compute-missing', '-c', action='store_true', dest='compute_missing', default=False,
+                        help='Attempt to compute missing feature values')
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
+    args = get_args()
+
     # Load session data
     session = database.create_session()
     tm_finder = TransitionMatchFinder()
@@ -84,7 +119,6 @@ if __name__ == '__main__':
     pairs_to_create = set()
     generate_tm_id_pairs_to_create()
     pairs_to_create = sorted(list(pairs_to_create), key=lambda x: x[0], reverse=True)
-    print('%d entries to create' % len(pairs_to_create))
 
     # Create DB rows
-    create_transition_match_smms_rows()
+    create_transition_match_smms_rows(session, args.compute_missing)