133cfdf736495923e5dd74086e7889c942ea01c0
alenlukic <alenlukic13@gmail.com>
2022-12-26 16:22:01 -0800

    spacing

--- src/scripts/feature_extraction/extract_features.sh
+++ src/scripts/feature_extraction/extract_features.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 cd /home/alen/Developer/dj-tools
-source venv/bin/activate
 
+source venv/bin/activate
 python src/scripts/feature_extraction/compute_mean_mel_spectrograms.py "$@"
 
 
 deactivate
 source venv/bin/activate
-
 python src/scripts/feature_extraction/create_transition_match_rows.py
